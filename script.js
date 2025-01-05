701bd44efc5f321942821f2ee76408fb2c172cf3
manu0619 <125184498+manu0619@users.noreply.github.com>
2025-01-05 19:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
+const arr = [1, 2, 3, 4, 1, 0, 2, 2]; 
 const divide = (arr, n) => {
-  // Write your code here
+    let res = [];
+    
+    while (arr.length > 0) {
+        let sum = 0;
+        let temp = []; // Temporary array to hold the current group
+
+        do {
+            let a = arr.shift();
+            sum += a;
+            temp.push(a);
+        } while (arr.length > 0 && sum + arr[0] <= n);
+
+        res.push(temp);
+    }
+
+    return res;
 };
+
 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
